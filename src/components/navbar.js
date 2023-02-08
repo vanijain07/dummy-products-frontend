1e449c303df51c0d504368433c99c8503c59f88e
vani <jain.vani@gmail.com>
2023-02-08 15:12:53 +0530

    toggler added

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -15,6 +15,17 @@ const Navbar = (props) => {
           <a className="navbar-brand" href="#">
             Dummy Products
           </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarTogglerDemo03"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item dropdown">
